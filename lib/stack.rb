5143fcf1d66b988ce3041b00883136fe4280dbbd
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-09-26 16:53:30 -0700

    commit tree d36ea44e3afcc4309814a010405263930963ccc7

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,41 @@
-# your code here
+class Stack
+
+  def initialize(limit = nil)
+    @stack = []
+    @limit = limit
+  end
+
+  def push(value)
+    raise 'Stack Overflow' if full?
+    @stack.push(value)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack.last
+  end
+
+  def size
+    @stack.length
+  end
+
+  def empty?
+    @stack.empty?
+  end
+
+  def search(value)
+    @stack.each_with_index do |item, idx|
+      return size - idx - 1 if item == value
+    end
+    -1
+  end
+
+  def full?
+    @limit && @stack.size == @limit
+  end
+
+
+end
